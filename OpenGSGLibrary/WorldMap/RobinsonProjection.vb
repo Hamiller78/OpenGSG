13516ee0fbe838eb6486302088f952e7def6aa3b
Hamiller78 <Kneesch@gmx.de>
2019-05-01 21:34:14 +0200

    Add map coordinates to geo coordinates conversion for Robinson projection (WIP)
    Not yet tested or optimized.

--- OpenGSGLibrary/WorldMap/RobinsonProjection.vb
+++ OpenGSGLibrary/WorldMap/RobinsonProjection.vb
@@ -25,6 +25,10 @@ Namespace Map
     Public Class RobinsonProjection
         Inherits Projection
 
+        ' constant parameters, Raidus = 1 and map centered around the Greenwich meridian
+        Const capitalR As Double = 1D
+        Const centralMeridian As Double = 0.0
+
         ''' <summary>
         ''' Returns the map coordinates based on a Robinson projection with R=1.
         ''' See: https://en.wikipedia.org/wiki/Robinson_projection
@@ -33,8 +37,6 @@ Namespace Map
         ''' <returns></returns>
         Public Overrides Function getMapCoordinates(globePoint As GeoCoordinate) As Tuple(Of Double, Double)
             Dim capitalXY As Tuple(Of Double, Double) = getXY(globePoint.Latitude)
-            Dim capitalR As Double = 1.0
-            Dim centralMeridian As Double = 0.0 'chosen central meridian for map
 
             Dim x As Double = 0.8487 * capitalR * capitalXY.Item1 * (globePoint.Longitude - centralMeridian)
             Dim y As Double = 1.3523 * capitalR * capitalXY.Item2
@@ -43,7 +45,13 @@ Namespace Map
         End Function
 
         Public Overrides Function getGlobeCoordinates(mapPoint As Tuple(Of Double, Double)) As GeoCoordinate
-            Throw New NotImplementedException()
+            Dim latitude As Double = 0D
+            Dim longitude As Double = 0D
+
+            latitude = getLatitudeOfY(mapPoint.Item2 / (1.3523 * capitalR))
+            longitude = mapPoint.Item1 / (0.8487 * capitalR * getXY(latitude).Item1) + centralMeridian
+
+            Return New GeoCoordinate(latitude, longitude)
         End Function
 
         Private Function getXY(latitude As Double) As Tuple(Of Double, Double)
@@ -105,6 +113,50 @@ Namespace Map
                 capitalY = linearInterpolation(latitude, 85, 0.9761, 90, 1)
             End If
             Return New Tuple(Of Double, Double)(capitalX, capitalY)
+        End Function
+
+        Private Function getLatitudeOfY(capitalY As Double) As Double
+            Dim latitude As Double = 0D
+
+            If (0 <= capitalY And capitalY < 0.062) Then
+                latitude = linearInterpolation(capitalY, 0, 0, 0.062, 5)
+            ElseIf (0.062 <= capitalY And capitalY < 0.124) Then
+                latitude = linearInterpolation(capitalY, 0.062, 5, 0.124, 10)
+            ElseIf (0.124 <= capitalY And capitalY < 0.186) Then
+                latitude = linearInterpolation(capitalY, 0.124, 10, 0.186, 15)
+            ElseIf (0.186 <= capitalY And capitalY < 0.248) Then
+                latitude = linearInterpolation(capitalY, 0.186, 15, 0.248, 20)
+            ElseIf (0.248 <= capitalY And capitalY < 0.31) Then
+                latitude = linearInterpolation(capitalY, 0.248, 20, 0.31, 25)
+            ElseIf (0.31 <= capitalY And capitalY < 0.372) Then
+                latitude = linearInterpolation(capitalY, 0.31, 25, 0.372, 30)
+            ElseIf (0.372 <= capitalY And capitalY < 0.434) Then
+                latitude = linearInterpolation(capitalY, 0.372, 30, 0.434, 35)
+            ElseIf (0.434 <= capitalY And capitalY < 0.4958) Then
+                latitude = linearInterpolation(capitalY, 0.434, 35, 0.4958, 40)
+            ElseIf (0.4958 <= capitalY And capitalY < 0.5571) Then
+                latitude = linearInterpolation(capitalY, 0.4958, 40, 0.5571, 45)
+            ElseIf (0.5571 <= capitalY And capitalY < 0.6176) Then
+                latitude = linearInterpolation(capitalY, 0.5571, 45, 0.6176, 50)
+            ElseIf (0.6176 <= capitalY And capitalY < 0.6769) Then
+                latitude = linearInterpolation(capitalY, 0.6176, 50, 0.6769, 55)
+            ElseIf (0.6769 <= capitalY And capitalY < 0.7346) Then
+                latitude = linearInterpolation(capitalY, 0.6769, 55, 0.7346, 60)
+            ElseIf (0.7346 <= capitalY And capitalY < 0.7903) Then
+                latitude = linearInterpolation(capitalY, 0.7346, 60, 0.7903, 65)
+            ElseIf (0.7903 <= capitalY And capitalY < 0.8435) Then
+                latitude = linearInterpolation(capitalY, 0.7903, 65, 0.8435, 70)
+            ElseIf (0.8435 <= capitalY And capitalY < 0.8936) Then
+                latitude = linearInterpolation(capitalY, 0.8435, 70, 0.8936, 75)
+            ElseIf (0.8936 <= capitalY And capitalY < 0.9394) Then
+                latitude = linearInterpolation(capitalY, 0.8936, 75, 0.9394, 80)
+            ElseIf (0.9394 <= capitalY And capitalY < 0.9761) Then
+                latitude = linearInterpolation(capitalY, 0.9394, 80, 0.9761, 85)
+            ElseIf (0.9761 <= capitalY And capitalY <= 1.0) Then
+                latitude = linearInterpolation(capitalY, 0.9761, 85, 1.0, 90)
+            End If
+
+            Return latitude
         End Function
 
         Private Function linearInterpolation(x As Double, x1 As Double, y1 As Double, x2 As Double, y2 As Double) As Double
