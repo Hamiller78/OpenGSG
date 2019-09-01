522a3e648fe0481c12dbae5c19168aa1748f64e4
Hamiller78 <Kneesch@gmx.de>
2019-09-01 19:54:32 +0200

    Add TickHandler class for time ticks (WIP)

---
+++ OpenGSGLibrary/GameLogic/TickHandler.vb
@@ -0,0 +1,40 @@
+﻿'    OpenSGSGLibrary is an open-source library for Grand Strategy Games
+'    Copyright (C) 2019  Torben Kneesch
+'
+'    This program is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+'
+'    This program is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Namespace GameLogic
+
+    Public Class TickHandler
+
+        Public Function IsTickComplete() As Boolean
+            ' tick time complete?
+            ' all players done?
+            Return True
+        End Function
+
+        Public Sub FinishTick()
+            ' lock GUI input
+            ' calculate world in next tick
+        End Sub
+
+        Public Sub BeginNewTick()
+            ' do timed game events
+            ' launch AI threads
+            ' collect GUI input
+        End Sub
+
+    End Class
+
+End Namespace
