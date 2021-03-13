f06a612c9878f333b45e194f597b162898e514c2
torben.kneesch <torben.kneesch@cgi.com>
2021-03-13 17:49:55 +0100

    Adds DevExpress project to display countries
    WIP: Should later also displays details and allow saving in database.

---
+++ DevExpressCountryManager/DevExpressCountryManager/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using OpenGSGLibrary.WorldData;
+using DevExpressCountryManager.WorldData;
+
+namespace DevExpressCountryManager
+{
+    public class MainWindowViewModel
+    {
+        private const string GAMEDATA_PATH = @"..\..\..\..\..\ColdWarPrototype\GameData";
+
+        public ObservableCollection<DXCountry> Countries { get; set; } = new ObservableCollection<DXCountry>();
+
+        private WorldLoader<Province, DXCountry> _worldLoader = new WorldLoader<Province, DXCountry>();
+
+        public MainWindowViewModel()
+        {
+            LoadCountries(GAMEDATA_PATH);
+        }
+
+        public void LoadCountries(string filepath)
+        {
+            try
+            {
+                WorldState loadedState = _worldLoader.CreateStartState(filepath);
+                Dictionary<string, Country> countryDictionary = (Dictionary<string, Country>)loadedState.GetCountryTable();
+                Countries.Clear();
+                foreach (KeyValuePair<string, Country> itemPair in countryDictionary)
+                {
+                    Countries.Add((DXCountry)(itemPair.Value));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception while loading data: " + ex.Message);
+                // TODO: Do something about the error
+            }
+        }
+    }
+}
---
+++ DevExpressCountryManager/DevExpressCountryManager/WorldData/DXCountry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenGSGLibrary.WorldData;
+
+namespace DevExpressCountryManager.WorldData
+{
+    public class DXCountry : Country
+    {
+
+
+        public override string ToString()
+        {
+            return GetName();
+        }
+    }
+}
