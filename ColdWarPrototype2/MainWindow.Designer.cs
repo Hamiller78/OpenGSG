7dbb855d7869c47833bfdd045ab3a2e93e3247ca
Torben Kneesch <Kneesch@gmx.de>
2025-11-29 17:54:14 +0100

    Correct migrated MainWindow layout

--- ColdWarPrototype2/MainWindow.Designer.cs
+++ ColdWarPrototype2/MainWindow.Designer.cs
@@ -7,10 +7,12 @@
         /// </summary>
         private System.ComponentModel.IContainer components = null;
         public System.Windows.Forms.PictureBox MapPictureBox;
+        public System.Windows.Forms.GroupBox MapModeGroup;
         public System.Windows.Forms.RadioButton MapModePolitical;
         public System.Windows.Forms.RadioButton MapModeRaw;
         public System.Windows.Forms.Button DateButton;
         public System.Windows.Forms.ListBox ArmyListBox;
+        public System.Windows.Forms.GroupBox ProvinceBox;
         public System.Windows.Forms.Label ProvinceName;
         public System.Windows.Forms.Label ProvincePopulation;
         public System.Windows.Forms.Label ProvinceIndustrialization;
@@ -20,8 +22,10 @@
         public System.Windows.Forms.Label ProvinceOwner;
         public System.Windows.Forms.Label ProvinceController;
 
+        public GroupBox CountryBox;
         public System.Windows.Forms.PictureBox FlagPictureBox;
         public System.Windows.Forms.Label CountryName;
+        public System.Windows.Forms.Label CountryProduction;
         public System.Windows.Forms.Label CountryLeader;
         public System.Windows.Forms.Label CountryGovernment;
         public System.Windows.Forms.Label CountryAllegiance;
@@ -47,57 +51,255 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1024, 768);
-            Text = "Cold War Prototype 2";
-
-            // Controls migrated from original UI
-            MapPictureBox = new PictureBox { Location = new Point(230, 0), Size = new Size(1654, 1000) };
-            MapPictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            MapPictureBox = new PictureBox();
+            MapModeGroup = new GroupBox();
+            MapModePolitical = new RadioButton();
+            MapModeRaw = new RadioButton();
+            DateButton = new Button();
+            ProvinceBox = new GroupBox();
+            ProvinceName = new Label();
+            ProvincePopulation = new Label();
+            ProvinceIndustrialization = new Label();
+            ProvinceEducation = new Label();
+            ProvinceProduction = new Label();
+            ProvinceTerrain = new Label();
+            ProvinceOwner = new Label();
+            ProvinceController = new Label();
+            FlagPictureBox = new PictureBox();
+            CountryBox = new GroupBox();
+            CountryProduction = new Label();
+            CountryName = new Label();
+            CountryLeader = new Label();
+            CountryGovernment = new Label();
+            CountryAllegiance = new Label();
+            ArmyListBox = new ListBox();
+            ((System.ComponentModel.ISupportInitialize)MapPictureBox).BeginInit();
+            MapModeGroup.SuspendLayout();
+            ProvinceBox.SuspendLayout();
+            CountryBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)FlagPictureBox).BeginInit();
+            SuspendLayout();
+            // 
+            // MapPictureBox
+            // 
             MapPictureBox.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-
-            MapModePolitical = new RadioButton { Location = new Point(10, 10), Text = "Political", Checked = true };
-            MapModeRaw = new RadioButton { Location = new Point(120, 10), Text = "Raw" };
-            DateButton = new Button { Location = new Point(240, 10), Text = "Date" };
-
-            // Province info labels (placed to the right of the map)
-            ProvinceName = new Label { Location = new Point(820, 10), Size = new Size(180, 20), Text = "" };
-            ProvincePopulation = new Label { Location = new Point(820, 30), Size = new Size(180, 20), Text = "" };
-            ProvinceIndustrialization = new Label { Location = new Point(820, 50), Size = new Size(180, 20), Text = "" };
-            ProvinceEducation = new Label { Location = new Point(820, 70), Size = new Size(180, 20), Text = "" };
-            ProvinceProduction = new Label { Location = new Point(820, 90), Size = new Size(180, 20), Text = "" };
-            ProvinceTerrain = new Label { Location = new Point(820, 110), Size = new Size(180, 20), Text = "" };
-            ProvinceOwner = new Label { Location = new Point(820, 130), Size = new Size(180, 20), Text = "" };
-            ProvinceController = new Label { Location = new Point(820, 150), Size = new Size(180, 20), Text = "" };
-
-            // Country info
-            FlagPictureBox = new PictureBox { Location = new Point(820, 180), Size = new Size(64, 40) };
-            CountryName = new Label { Location = new Point(890, 180), Size = new Size(110, 20), Text = "" };
-            CountryLeader = new Label { Location = new Point(890, 200), Size = new Size(110, 20), Text = "" };
-            CountryGovernment = new Label { Location = new Point(890, 220), Size = new Size(110, 20), Text = "" };
-            CountryAllegiance = new Label { Location = new Point(890, 240), Size = new Size(110, 20), Text = "" };
-
-            ArmyListBox = new ListBox { Location = new Point(820, 260), Size = new Size(180, 380) };
-
+            MapPictureBox.Location = new Point(230, 0);
+            MapPictureBox.Name = "MapPictureBox";
+            MapPictureBox.Size = new Size(1654, 1000);
+            MapPictureBox.TabIndex = 0;
+            MapPictureBox.TabStop = false;
+            // 
+            // MapModeRaw
+            //
+            MapModeRaw.AutoSize = true;
+            MapModeRaw.Checked = true;
+            MapModeRaw.Location = new Point(8, 19);
+            MapModeRaw.Name = "MapModeRaw";
+            MapModeRaw.Size = new Size(47, 17);
+            MapModeRaw.TabIndex = 2;
+            MapModeRaw.TabStop = true;
+            MapModeRaw.Text = "Raw";
+            MapModeRaw.UseVisualStyleBackColor = true;
+            // 
+            // MapModePolitical
+            //
+            MapModePolitical.AutoSize = true;
+            MapModePolitical.Location = new Point(8, 42);
+            MapModePolitical.Name = "MapModePolitical";
+            MapModePolitical.Size = new Size(61, 17);
+            MapModePolitical.TabIndex = 1;
+            MapModePolitical.Text = "Political";
+            MapModeRaw.UseVisualStyleBackColor = true;
+            //
+            // MapModeGroup
+            //
+            MapModeGroup.Controls.Add(MapModePolitical);
+            MapModeGroup.Controls.Add(MapModeRaw);
+            MapModeGroup.Location = new Point(4, 3);
+            MapModeGroup.Name = "MapModeGroup";
+            MapModeGroup.Size = new Size(220, 66);
+            MapModeGroup.TabIndex = 3;
+            MapModeGroup.TabStop = false;
+            MapModeGroup.Text = "Map Modes";
+            // 
+            // DateButton
+            // 
+            DateButton.Location = new Point(4, 952);
+            DateButton.Name = "DateButton";
+            DateButton.Size = new Size(216, 37);
+            DateButton.TabIndex = 3;
+            DateButton.Text = "DD-MM-YYYY";
+            DateButton.UseVisualStyleBackColor = true;
+            //
+            // ProvinceBox
+            //
+            ProvinceBox.Controls.Add(ProvinceTerrain);
+            ProvinceBox.Controls.Add(ProvinceProduction);
+            ProvinceBox.Controls.Add(ProvinceEducation);
+            ProvinceBox.Controls.Add(ProvinceOwner);
+            ProvinceBox.Controls.Add(ProvinceController);
+            ProvinceBox.Controls.Add(ProvinceIndustrialization);
+            ProvinceBox.Controls.Add(ProvincePopulation);
+            ProvinceBox.Controls.Add(ProvinceName);
+            ProvinceBox.Location = new Point(4, 77);
+            ProvinceBox.Name = "ProvinceBox";
+            ProvinceBox.Size = new Size(224, 156);
+            ProvinceBox.TabIndex = 4;
+            ProvinceBox.TabStop = false;
+            ProvinceBox.Text = "Province";
+            // 
+            // ProvinceName
+            // 
+            ProvinceName.Location = new Point(8, 18);
+            ProvinceName.Name = "ProvinceName";
+            ProvinceName.Size = new Size(10, 13);
+            ProvinceName.TabIndex = 4;
+            ProvinceName.Text = "-";
+            // 
+            // ProvincePopulation
+            // 
+            ProvincePopulation.Location = new Point(8, 38);
+            ProvincePopulation.Name = "ProvincePopulation";
+            ProvincePopulation.Size = new Size(13, 13);
+            ProvincePopulation.TabIndex = 5;
+            ProvincePopulation.Text = "0";
+            // 
+            // ProvinceIndustrialization
+            // 
+            ProvinceIndustrialization.Location = new Point(8, 57);
+            ProvinceIndustrialization.Name = "ProvinceIndustrialization";
+            ProvinceIndustrialization.Size = new Size(10, 13);
+            ProvinceIndustrialization.TabIndex = 6;
+            ProvinceIndustrialization.Text = "-";
+            // 
+            // ProvinceEducation
+            // 
+            ProvinceEducation.Location = new Point(8, 70);
+            ProvinceEducation.Name = "ProvinceEducation";
+            ProvinceEducation.Size = new Size(10, 13);
+            ProvinceEducation.TabIndex = 7;
+            ProvinceEducation.Text = "-";
+            // 
+            // ProvinceProduction
+            // 
+            ProvinceProduction.Location = new Point(8, 115);
+            ProvinceProduction.Name = "ProvinceProduction";
+            ProvinceProduction.Size = new Size(10, 13);
+            ProvinceProduction.TabIndex = 8;
+            ProvinceProduction.Text = "-";
+            // 
+            // ProvinceTerrain
+            // 
+            ProvinceTerrain.Location = new Point(8, 128);
+            ProvinceTerrain.Name = "ProvinceTerrain";
+            ProvinceTerrain.Size = new Size(10, 13);
+            ProvinceTerrain.TabIndex = 9;
+            ProvinceTerrain.Text = "-";
+            // 
+            // ProvinceOwner
+            // 
+            ProvinceOwner.Location = new Point(8, 83);
+            ProvinceOwner.Name = "ProvinceOwner";
+            ProvinceOwner.Size = new Size(10, 13);
+            ProvinceOwner.TabIndex = 10;
+            ProvinceOwner.Text = "-";
+            // 
+            // ProvinceController
+            // 
+            ProvinceController.Location = new Point(8, 96);
+            ProvinceController.Name = "ProvinceController";
+            ProvinceController.Size = new Size(10, 13);
+            ProvinceController.TabIndex = 11;
+            ProvinceController.Text = "-";
+            //
+            // CountryBox
+            //
+            CountryBox.Controls.Add(FlagPictureBox);
+            CountryBox.Controls.Add(CountryProduction);
+            CountryBox.Controls.Add(CountryAllegiance);
+            CountryBox.Controls.Add(CountryGovernment);
+            CountryBox.Controls.Add(CountryLeader);
+            CountryBox.Controls.Add(CountryName);
+            CountryBox.Location = new Point(4, 239);
+            CountryBox.Name = "CountryBox";
+            CountryBox.Size = new Size(224, 93);
+            CountryBox.TabIndex = 5;
+            CountryBox.TabStop = false;
+            CountryBox.Text = "Country";
+            //
+            // FlagPictureBox
+            // 
+            FlagPictureBox.Location = new Point(174, 16);
+            FlagPictureBox.Name = "FlagPictureBox";
+            FlagPictureBox.Size = new Size(45, 30);
+            FlagPictureBox.TabIndex = 12;
+            FlagPictureBox.TabStop = false;
+            // 
+            // CountryName
+            // 
+            CountryName.Location = new Point(8, 16);
+            CountryName.Name = "CountryName";
+            CountryName.Size = new Size(10, 13);
+            CountryName.TabIndex = 13;
+            CountryName.Text = "-";
+            // 
+            // CountryLeader
+            // 
+            CountryLeader.Location = new Point(8, 29);
+            CountryLeader.Name = "CountryLeader";
+            CountryLeader.Size = new Size(10, 13);
+            CountryLeader.TabIndex = 14;
+            CountryLeader.Text = "";
+            // 
+            // CountryGovernment
+            // 
+            CountryGovernment.Location = new Point(8, 42);
+            CountryGovernment.Name = "CountryGovernment";
+            CountryGovernment.Size = new Size(10, 13);
+            CountryGovernment.TabIndex = 15;
+            CountryGovernment.Text = "";
+            // 
+            // CountryAllegiance
+            // 
+            CountryAllegiance.Location = new Point(8, 55);
+            CountryAllegiance.Name = "CountryAllegiance";
+            CountryAllegiance.Size = new Size(10, 13);
+            CountryAllegiance.TabIndex = 16;
+            CountryAllegiance.Text = "";
+            // 
+            // ArmyListBox
+            // 
+            ArmyListBox.Location = new Point(4, 341);
+            ArmyListBox.Name = "ArmyListBox";
+            ArmyListBox.Size = new Size(223, 199);
+            ArmyListBox.TabIndex = 17;
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1884, 1001);
             Controls.Add(MapPictureBox);
-            Controls.Add(MapModePolitical);
-            Controls.Add(MapModeRaw);
+            Controls.Add(MapModeGroup);
             Controls.Add(DateButton);
-            Controls.Add(ProvinceName);
-            Controls.Add(ProvincePopulation);
-            Controls.Add(ProvinceIndustrialization);
-            Controls.Add(ProvinceEducation);
-            Controls.Add(ProvinceProduction);
-            Controls.Add(ProvinceTerrain);
-            Controls.Add(ProvinceOwner);
-            Controls.Add(ProvinceController);
-            Controls.Add(FlagPictureBox);
-            Controls.Add(CountryName);
-            Controls.Add(CountryLeader);
-            Controls.Add(CountryGovernment);
-            Controls.Add(CountryAllegiance);
             Controls.Add(ArmyListBox);
+            Controls.Add(ProvinceBox);
+            Controls.Add(CountryBox);
+            //Controls.Add(CoordinateDisplay);
+            //Controls.Add(MoveArmiesButton);
+            Name = "MainWindow";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Cold War Strategy Prototype";
+            ((System.ComponentModel.ISupportInitialize)MapPictureBox).EndInit();
+            MapModeGroup.ResumeLayout(false);
+            MapModeGroup.PerformLayout();
+            ProvinceBox.ResumeLayout(false);
+            ProvinceBox.PerformLayout();
+            CountryBox.ResumeLayout(false);
+            CountryBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)FlagPictureBox).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
--- ColdWarPrototype2/Views/ArmyList.cs
+++ ColdWarPrototype2/Views/ArmyList.cs
@@ -8,10 +8,16 @@ namespace ColdWarPrototype2.Views
     /// <summary>
     /// Simple view helper for showing armies in a province.
     /// Provides methods to get armies for a province and to fill a ListBox control.
+    /// Also supports updating the internal motherWindow's ArmyListBox like the original VB view.
     /// </summary>
     public class ArmyList
     {
         private readonly MainWindow motherWindow_;
+
+        private int currentProvinceId_;
+        private bool isChoosingTarget_ = false;
+        private List<Army> armiesInProvince_ = new List<Army>();
+        private List<Army> selectedArmies_ = new List<Army>();
 
         public ArmyList(MainWindow motherWindow)
         {
@@ -26,7 +32,7 @@ namespace ColdWarPrototype2.Views
             var mgr = state?.GetArmyManager();
             if (mgr == null)
                 return new List<Army>();
-            return mgr.GetArmiesInProvince(provinceId);
+            return mgr.GetArmiesInProvince(provinceId) ?? new List<Army>();
         }
 
         /// <summary>
@@ -41,6 +47,30 @@ namespace ColdWarPrototype2.Views
             {
                 listBox.Items.Add(army);
             }
+        }
+
+        /// <summary>
+        /// Updates the mother window's ArmyListBox with armies from the given province.
+        /// Mirrors the original VB behaviour which used BeginUpdate/EndUpdate and stored internal state.
+        /// </summary>
+        public void UpdateArmyListBox(WorldState currentState, int mouseProvinceId)
+        {
+            armiesInProvince_ = currentState?.GetArmyManager()?.GetArmiesInProvince(mouseProvinceId) ?? new List<Army>();
+
+            if (motherWindow_?.ArmyListBox == null)
+                return;
+
+            var lb = motherWindow_.ArmyListBox;
+            lb.Items.Clear();
+            lb.BeginUpdate();
+            if (armiesInProvince_ != null)
+            {
+                foreach (var army in armiesInProvince_)
+                {
+                    lb.Items.Add(army.ToString());
+                }
+            }
+            lb.EndUpdate();
         }
     }
 }
